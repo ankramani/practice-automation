6dbdddde5a2dacace352f12358e82c2d5312048c
Ankit Ramani <ankit.ramani@FORGEAHEAD.IO>
2017-05-30 17:30:56 +0530

    modified initiate

--- src/main/java/Initiate.java
+++ src/main/java/Initiate.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
+import org.testng.annotations.BeforeSuite;
 
 /**
  * Created by ankit.ramani on 5/30/2017.
  */
-public class Initiate {
+public class Initiate extends CommonUtil{
+
+    @BeforeSuite
+    public void startPoint(){
+        CommonUtil.launchBrowser();
+        CommonUtil.accessUrl("https://www.google.com");
+    }
+
+
+
+
 }
