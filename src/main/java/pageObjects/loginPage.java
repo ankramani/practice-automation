f7b2240277a5cc1b5e02aee411405da4a33836a8
Ankit Ramani <ankit.ramani@FORGEAHEAD.IO>
2017-06-06 15:46:04 +0530

    created loginpage.java

--- src/main/java/pageObjects/loginPage.java
+++ src/main/java/pageObjects/loginPage.java
@@ -1,7 +1,24 @@
 package pageObjects;
 
+import browserAction.CommonUtil;
+import org.openqa.selenium.By;
+
 /**
  * Created by ankit.ramani on 6/6/2017.
  */
-public class loginPage {
+public class loginPage extends CommonUtil {
+
+    static By username = By.id("username");
+    static By password = By.name("pwd");
+    static By login = By.id("loginButton");
+    public static void enterUsername(){
+        CommonUtil.enterText(username, "user");
+    }
+
+    public static void enterPassword(){
+        CommonUtil.enterText(password, "user");
+    }
+    public static void clickLogin(){
+        CommonUtil.click(login);
+    }
 }
