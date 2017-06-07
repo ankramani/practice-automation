f768c1025082fb85641dd9dc2084a5e655e2a427
Ankit Ramani <ankit.ramani1@gmail.com>
2017-06-07 16:46:16 +0530

    mod commonutil

--- src/main/java/browserAction/CommonUtil.java
+++ src/main/java/browserAction/CommonUtil.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by ankit.ramani on 5/29/2017.
  */
@@ -24,11 +26,16 @@ public class CommonUtil {
     public static void enterText(By by, String s){
         driver.findElement(by).sendKeys(s);
     }
-
+    public static void iwait(){
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
     public static void accessUrl(String url){
         driver.get(url);
     }
     public static void closeBrowser(){
         driver.close();
+    }
+    public static void iFrame(String s){
+        driver.switchTo().frame(s);
     }
 }
