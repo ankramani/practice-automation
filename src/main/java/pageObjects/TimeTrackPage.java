68aed61b99bce26bc34081e47265caee3448a67b
Ankit Ramani <ankit.ramani1@gmail.com>
2017-06-08 18:08:31 +0530

    added timetrackpage

--- src/main/java/pageObjects/TimeTrackPage.java
+++ src/main/java/pageObjects/TimeTrackPage.java
@@ -1,7 +1,16 @@
 package pageObjects;
 
+import browserAction.CommonUtil;
+import org.openqa.selenium.By;
+
 /**
  * Created by ankit.ramani on 6/8/2017.
  */
 public class TimeTrackPage {
+
+    static By clicknew = By.xpath("//span[text() = 'New']");
+
+    public static void clickNewbutton(){
+        CommonUtil.click(clicknew);
+    }
 }
