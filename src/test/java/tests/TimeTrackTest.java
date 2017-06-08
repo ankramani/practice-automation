283c8382d0cde99f82065e1aedcc5ef1a338788d
Ankit Ramani <ankit.ramani1@gmail.com>
2017-06-08 18:09:24 +0530

    added timetracktest

--- src/test/java/tests/TimeTrackTest.java
+++ src/test/java/tests/TimeTrackTest.java
@@ -1,7 +1,15 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pageObjects.TimeTrackPage;
+
 /**
  * Created by ankit.ramani on 6/8/2017.
  */
 public class TimeTrackTest {
+
+    @Test
+    public static void timeTrack(){
+        TimeTrackPage.clickNewbutton();
+    }
 }
