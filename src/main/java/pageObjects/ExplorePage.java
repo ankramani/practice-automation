95eea54e4f26734e54cb9c9b62b13e7aed8ceb17
Ankit Ramani <ankit.ramani1@gmail.com>
2017-06-08 18:08:01 +0530

    mod explorepage

--- src/main/java/pageObjects/ExplorePage.java
+++ src/main/java/pageObjects/ExplorePage.java
@@ -6,15 +6,19 @@ import org.openqa.selenium.By;
 /**
  * Created by ankit.ramani on 6/7/2017.
  */
-public class Explore {
+public class ExplorePage {
 
 //    static By playerframe = By.id("videoPlayerId");
-    static  By vplayer = By.id("player");
+    static By vplayer = By.id("player");
+    static By expbut = By.xpath("//span[text() = 'Start exploring actiTIME']");
 
     /*public static void switchFrame(){
         CommonUtil.iFrame(playerframe);
     }*/
     public static void videoPlay(){
         CommonUtil.click(vplayer);
+    }
+    public static void exploreButton(){
+        CommonUtil.click(expbut);
     }
 }
---
+++ src/test/java/tests/TimeTrackTest.java
@@ -0,0 +1,7 @@
+package tests;
+
+/**
+ * Created by ankit.ramani on 6/8/2017.
+ */
+public class TimeTrackTest {
+}
